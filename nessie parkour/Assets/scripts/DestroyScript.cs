c17a4052a08cd8723a810c3ba13c219dbd5fc693
MarithzaR <marithza1233@gmail.com>
2023-05-30 13:07:04 -0700

    udshilsdlhsd

---
+++ nessie parkour/Assets/scripts/DestroyScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Debug.Log (transform.position);
+	}
+	void OnCollisionEnter2D(Collision2D collision){
+		//Destroy (this.gameObject);
+		Debug.Log (collision.gameObject.name);
+		if (collision.gameObject.name != "Player") {
+			Destroy (this.gameObject);
+		}
+	}
+}
